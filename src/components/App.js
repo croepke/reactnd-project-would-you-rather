739538c7db29cf6342bf89bb5853a958e146b886
Christian Röpke <croepke@posteo.de>
2018-05-26 07:40:21 +0200

    feat: Create base components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        Hello world!
+      </div>
+    );
+  }
+}
+
+export default App;
