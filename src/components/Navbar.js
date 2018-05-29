05e5b181cfb7a56e8b7330d03f81739d7ede45ea
Christian Röpke <croepke@posteo.de>
2018-05-29 07:02:03 +0200

    feat: Create basic navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-    <div>Nav</div>
+  <div>
+    <div>
+      <Link to='/'>Home</Link>
+      <Link to='/add'>New poll</Link>
+      <Link to='/leaderboard'>Leaderboard</Link>
+    </div>
+    <div>
+      Login
+    </div>
+  </div>
 );
 
 export default Navbar;
