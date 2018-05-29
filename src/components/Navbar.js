7fdf69559ad00ffb8fc5b5f9ed7e0bdcb62161c3
Christian Röpke <croepke@posteo.de>
2018-05-29 07:09:59 +0200

    feat: Create column layout for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <div>
-    <div>
+  <div className='columns'>
+    <div className='column'>
       <Link to='/'>Home</Link>
       <Link to='/add'>New poll</Link>
       <Link to='/leaderboard'>Leaderboard</Link>
     </div>
-    <div>
+    <div className='column is-narrow'>
       Login
     </div>
   </div>
