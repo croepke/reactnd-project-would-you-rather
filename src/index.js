37bfa8705f5daf47d47e76f8988ff623b2144d92
Christian Röpke <croepke@posteo.de>
2018-06-05 07:39:57 +0200

    feat: Create store and add reducers

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
 import './index.css';
 import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
