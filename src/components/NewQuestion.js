3229bd66cbd916176654a20ddc8c3ba50ca301eb
Christian Röpke <croepke@posteo.de>
2018-06-15 14:50:05 +0200

    feat: Add basic question form

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const NewQuestion = () => (
-    <div>New Question</div>
-);
+class NewQuestion extends Component {
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className='content container'>
+        <h1>Would you rather ... ?</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className='columns'>
+            <div className='column'>
+              <div className='field'>
+                <div className='control'>
+                  <input className='input' type='text' placeholder='Option one'/>
+                </div>
+              </div>
+            </div>
+            <div className='column is-narrow'>
+              <span>or</span>
+            </div>
+            <div className='column'>
+              <div className='field'>
+                <div className='control'>
+                  <input className='input' type='text' placeholder='Option two'/>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <button type='submit' className='button is-info'>Create question</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
 
 export default NewQuestion;
